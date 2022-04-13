049cc7aea1584b32ffb67fe89f5727c2e5498439
alptarla <alptarla@gmail.com>
2022-04-13 08:37:03 +0300

    Add theme data to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/core/app_theme.dart';
 import 'package:shopping_cart/view/cart_list_view.dart';
 import 'package:shopping_cart/view/product_list_view.dart';
 
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Shopping Cart",
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
       initialRoute: ProductListView.route,
       routes: {
         ProductListView.route: (context) => const ProductListView(),
