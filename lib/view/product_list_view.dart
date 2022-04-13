7e288e322f38e1c28e3735deaf709e45b5361839
alptarla <alptarla@gmail.com>
2022-04-13 11:59:46 +0300

    List product cards

--- lib/view/product_list_view.dart
+++ lib/view/product_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shopping_cart/model/product_model.dart';
 import 'package:shopping_cart/view_model/product_view_model.dart';
 
 class ProductListView extends StatefulWidget {
@@ -25,12 +26,41 @@ class _ProductListViewState extends State<ProductListView> {
       ),
       body: Consumer<ProductViewModel>(
         builder: (context, value, child) {
-          bool isLoading = context.watch<ProductViewModel>().isLoading;
-          return Center(
-            child: Text(isLoading ? "loading" : "idle"),
-          );
+          bool isLoading = context
+              .select<ProductViewModel, bool>((value) => value.isLoading);
+          return isLoading ? _buildSpinner() : _buildProductList(context);
         },
       ),
+    );
+  }
+
+  ListView _buildProductList(BuildContext context) {
+    List<ProductModel> products = context.watch<ProductViewModel>().products;
+
+    return ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return _buildProductCard(product: products[index]);
+      },
+    );
+  }
+
+  ListTile _buildProductCard({required ProductModel product}) {
+    return ListTile(
+      title: Text(product.title!),
+      leading: Image.network(
+        product.image!,
+        width: 50,
+        height: 50,
+        fit: BoxFit.contain,
+      ),
+      trailing: Text(product.price!.toString()),
+    );
+  }
+
+  Center _buildSpinner() {
+    return const Center(
+      child: CircularProgressIndicator(),
     );
   }
 }
